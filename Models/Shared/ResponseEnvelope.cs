d7973e6d5c043061b1e06aa97709733fa0b8e5d3
Kevin Schreiner <kschreiner@nationsphotolab.com>
2020-08-18 15:40:58 -0400

    Adjusted Response Envelope logic. The Purchase Create was throwing an exception because Yotpo has an inconsisten result structure. The new logic takes a code/message response and adjusts to match the expected result envelope.

--- Models/Shared/ResponseEnvelope.cs
+++ Models/Shared/ResponseEnvelope.cs
@@ -15,10 +15,33 @@ namespace Yotpo.Light.Models.Shared
         public ResponsePagination pagination { get; set; }
         public object products { get; set; }
 
+        protected class ActualResponseEnvelop
+        {
+            public ResponseStatus status { get; set; }
+            public object response { get; set; }
+            public ResponsePagination pagination { get; set; }
+            public object products { get; set; }
+
+            public int? code { get; set; }
+            public string message { get; set; }
+        }
+
         public static ResponseEnvelope from(HttpResponseMessage response)
         {
-            return JsonConvert.DeserializeObject<ResponseEnvelope>(response.Content.ReadAsStringAsync().Result);
+            var r = JsonConvert.DeserializeObject<ActualResponseEnvelop>(response.Content.ReadAsStringAsync().Result);
+            var ar = new ResponseEnvelope();
+            ar.status = r.status;
+            ar.response = r.response;
+            ar.pagination = r.pagination;
+            ar.products = r.products;
+            if (ar.status==null && r.code!=null)
+            {
+                ar.status = new ResponseStatus();
+                ar.status.code = r.code.Value;
+                ar.status.message = r.message;
+            }
+            return ar;
         }
     }
-    
+
 }
